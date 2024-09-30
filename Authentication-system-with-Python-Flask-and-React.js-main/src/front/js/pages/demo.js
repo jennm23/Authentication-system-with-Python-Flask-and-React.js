57c3da6fc1e9f0ed65be94f4b61532cd734bc78a
Jeneidys <jeneidys.23@gmail.com>
2024-09-30 06:54:35 -0300

    Primer Commit

---
+++ Authentication-system-with-Python-Flask-and-React.js-main/src/front/js/pages/demo.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import { Context } from "../store/appContext";
+
+export const Demo = () => {
+	const { store, actions } = useContext(Context);
+
+	const navigate = useNavigate();
+	useEffect(()=> {
+		var myHeaders = new Headers();
+		myHeaders.append("Authorization", "Bearer "+localStorage.getItem("token"));
+
+		var requestOptions = {
+		method: 'GET',
+		headers: myHeaders,
+		redirect: 'follow'
+		};
+
+		fetch("https://3001-4geeksacade-reactflaskh-ea2apv1hwm8.ws-eu79.gitpod.io/api/private", requestOptions)
+		.then(response => response.json())
+		.then(result => {
+			if(!result.ok) {
+				navigate("/")
+			}
+		})
+		.catch(error => console.log('error', error));
+	}, [])
+
+	return (
+		<div className="container">
+			<ul className="list-group">
+				{store.demo.map((item, index) => {
+					return (
+						<li
+							key={index}
+							className="list-group-item d-flex justify-content-between"
+							style={{ background: item.background }}>
+							<Link to={"/single/" + index}>
+								<span>Link to: {item.title}</span>
+							</Link>
+							{
+							item.background === "orange" ? (
+								<p style={{ color: item.initial }}>
+									Check store/flux.js scroll to the actions to see the code
+								</p>
+							) : null}
+							<button className="btn btn-success" onClick={() => actions.changeColor(index, "orange")}>
+								Change Color
+							</button>
+						</li>
+					);
+				})}
+			</ul>
+			<br />
+			<Link to="/">
+				<button className="btn btn-primary">Back home</button>
+			</Link>
+			<button>Logout </button>
+		</div>
+	);
+};
---
+++ Authentication-system-with-Python-Flask-and-React.js-main/src/front/js/pages/home.js
@@ -0,0 +1,63 @@
+import React, { useContext, useState } from "react";
+import { Context } from "../store/appContext";
+import rigoImageUrl from "../../img/rigo-baby.jpg";
+import "../../styles/home.css";
+import { Link, useNavigate } from "react-router-dom";
+
+export const Home = () => {
+	const { store, actions } = useContext(Context);
+
+	const navigate = useNavigate();
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [error, setError] = useState("");
+	
+	const login = () => {
+		console.log(email, password)
+		var myHeaders = new Headers();
+			myHeaders.append("Content-Type", "application/json");
+
+		var raw = JSON.stringify({
+			"email": email,
+			"password": password
+		});
+
+		var requestOptions = {
+			method: 'POST',
+			headers: myHeaders,
+			body: raw,
+			redirect: 'follow'
+		};
+
+		fetch("https://3001-4geeksacade-reactflaskh-ea2apv1hwm8.ws-eu79.gitpod.io/api/login", requestOptions)
+		.then(response => response.json())
+		.then(result => {
+			if(result.token) {
+				localStorage.setItem("token", result.token);
+				navigate("/demo");
+			}
+			else{ 
+				setError(result.msg)};
+		})
+		
+		.catch(error => console.log('error', error));}
+
+	return (
+		<div className="text-center mt-5">
+			<h1>LOGIN</h1>			
+			<p>
+				<label class="form-label">Email:</label>
+				<input class="form-control" onChange={(event)=> setEmail(event.target.value)}></input>
+			</p>
+			<p>
+				<label class="form-label">Password:</label>
+				<input class="form-control" onChange={(event)=> setPassword(event.target.value)}></input>
+			</p>
+			<button type="button" class="btn btn-outline-primary m-3" onClick={login}>Login</button>
+			<button type="button" class="btn btn-outline-primary m-3"><Link to={"/signup"}>Signup</Link></button>
+			{error && <div class="alert alert-danger" role="alert">
+				{error}
+				</div>}
+		</div>
+	);
+};
